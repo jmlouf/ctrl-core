e552c0aa486d5fd2b022e2f4db18e51b44242d8f
brutchley <bhutchison0901@yahoo.com>
2024-03-04 19:43:17 -0600

    added model for Project and made sure that this is succesfully integerated into model for user. working on comments next

---
+++ server/models/Project.js
@@ -0,0 +1,34 @@
+//still need to create dateFormat file in Utils folder
+
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const projectSchema = new Schema({
+    projectLink: {
+        type: String,
+        required: 'Must be a valid link to webpage or source code',
+        minlength: 1,
+        maxlength: 500,
+    },
+    githubLink: {
+        type: String,
+        required: 'Must be a valid link to GitHub repository',
+        minlength: 1,
+        maxlength: 500,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => dateFormat(timestamp),
+    },
+    comments: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Comments',
+        },
+    ],
+});
+
+const Project = model('Project', projectSchema);
+
+module.exports = Project;
--- server/models/User.js
+++ server/models/User.js
@@ -1,23 +1,30 @@
-const mongoose = require('mongoose');
-
-const { Schema } = mongoose;
+const { Schema, model } = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
     username: {
         type: String,
-        required: true
+        required: true,
+        unique: true,
+        trim: true,
     },
     email: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
+        match: [/.+@.+\..+/, 'Not a valid email!'],
     },
     password: {
         type: String,
         required: true,
         minlength: 8
-    }
+    },
+    projects: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Project',
+        },
+    ],
 });
 
 userSchema.pre('save', async function(next) {
