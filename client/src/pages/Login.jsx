46dedd913c5bde04e43ca380a090230ce2e83116
joem3847 <joem3847@gmail.com>
2024-03-05 20:05:51 -0600

    Add client authentication

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,36 +1,101 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useMutation } from "@apollo/client";
+import { LOGIN_USER } from "../utils/mutations";
 
-function Login() {
+import Auth from "../utils/auth";
+
+const Login = () => {
+  // Initialize email and password as empty strings with useState hook.
+  const [formState, setFormState] = useState({ email: "", password: "" });
+  // Define login mutation function with error and data properties from mutation result.
+  const [login, { error, data }] = useMutation(LOGIN_USER);
+
+  // Update state based on form input changes.
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      // Spread syntax for existing state.
+      ...formState,
+      // Update specific field with new value.
+      [name]: value
+    });
+  };
+
+  // Submit form.
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    console.log(formState);
+
+    try {
+      // Call login mutation function with formState variables.
+      const { data } = await login({
+        variables: { ...formState }
+      });
+
+      // Get token from response and pass to Auth.login() to log in user.
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+    }
+
+    // Clear form values.
+    setFormState({
+      email: "",
+      password: ""
+    });
+  };
+
   return (
-    <div className='container my-1'>
-      <Link to='/signup'>Don&apos;t have an account? Signup here!</Link>
+    <main className='flex-row justify-center mb-4'>
+      <div className='col-12 col-lg-10'>
+        <div className='card'>
+          <h4 className='card-header bg-dark text-light p-2'>Login</h4>
+          <div className='card-body'>
+            {data ? (
+              <p>
+                Success! You may now head
+                <Link to='/'>back to the homepage.</Link>
+              </p>
+            ) : (
+              <form onSubmit={handleFormSubmit}>
+                <input
+                  className='form-input'
+                  placeholder='Enter Email'
+                  name='email'
+                  type='email'
+                  value={formState.email}
+                  onChange={handleChange}
+                />
+                <input
+                  className='form-input'
+                  placeholder='Enter Password'
+                  name='password'
+                  type='password'
+                  value={formState.password}
+                  onChange={handleChange}
+                />
+                <button
+                  className='btn btn-block btn-primary'
+                  style={{ cursor: "pointer" }}
+                  type='submit'
+                >
+                  Submit
+                </button>
+              </form>
+            )}
 
-      <h2>Login</h2>
-      <form>
-        <div className='flex-row space-between my-2'>
-          <label htmlFor='email'>Email:</label>
-          <input
-            placeholder='Enter Email'
-            name='email'
-            type='email'
-            id='email'
-          />
+            {error && (
+              <div className='my-3 p-3 bg-danger text-white'>
+                {error.message}
+              </div>
+            )}
+          </div>
         </div>
-        <div className='flex-row space-between my-2'>
-          <label htmlFor='pwd'>Password:</label>
-          <input
-            placeholder='Enter Password'
-            name='password'
-            type='password'
-            id='pwd'
-          />
-        </div>
-        <div className='flex-row flex-end'>
-          <button type='submit'>Submit</button>
-        </div>
-      </form>
-    </div>
+      </div>
+    </main>
   );
-}
+};
 
 export default Login;
---
+++ client/src/utils/auth.js
@@ -0,0 +1,53 @@
+import decode from "jwt-decode";
+
+// Encapsulate methods for authentication.
+class AuthService {
+  getProfile() {
+    // Get user profile info by decoding JWT from getToken().
+    return decode(this.getToken());
+  }
+
+  loggedIn() {
+    // Checks if user is logged in via token existence and its expiration.
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token);
+  }
+
+  // Checks if token is expired.
+  isTokenExpired(token) {
+    try {
+      // Decode token.
+      const decoded = decode(token);
+      // Compare token's expiration with current time.
+      // If expiration is less than current timestamp, expired => returns true.
+      if (decoded.exp < Date.now() / 1000) {
+        return true;
+      } else return false;
+    } catch (err) {
+      return false;
+    }
+  }
+
+  getToken() {
+    // Retrieves the user token from localStorage.
+    // Uses key => 'id_token'.
+    return localStorage.getItem("id_token");
+  }
+
+  login(idToken) {
+    // Saves user token to localStorage.
+    // Redirects user to root URL ('/').
+    localStorage.setItem("id_token", idToken);
+
+    window.location.assign("/");
+  }
+
+  logout() {
+    // Clear user token and profile data from localStorage.
+    localStorage.removeItem("id_token");
+    // Reload page and reset the state of the application.
+    window.location.assign("/");
+  }
+}
+
+export default new AuthService();
